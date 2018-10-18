90edb01636def7e48c15b916ba263568b2488f22
Serj-M <dus-f@mail.ru>
2018-10-18 15:22:05 +0300

    Add all project

---
+++ DNS/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace DNS
+{
+    /*class Test
+    {
+        static void Main(string[] args)
+        {
+            string ip = "188.120.38.28";
+            IPAddress addr = IPAddress.Parse(ip);
+            IPHostEntry entry = Dns.GetHostEntry(addr);
+            MessageBox.Show(entry.HostName);
+        }
+    }*/
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string ip = "10.10.17.4";
+            IPAddress addr = IPAddress.Parse(ip);
+            IPHostEntry entry = Dns.GetHostEntry(addr);
+            MessageBox.Show(entry.HostName);
+
+        }
+    }
+}
